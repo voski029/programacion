002478e228eb79fe7ff715d8f8f7ff12f05d5a83
voski029 <diamamail557@gmail.com>
2024-11-13 19:36:44 +0100

    Add files via upload

---
+++ Tema1Ejercicio5.java
@@ -0,0 +1,46 @@
+package Tema1;
+
+public class Tema1Ejercicio5 {
+    public static void main(String[] args) {
+        // Realizar operacion 24 % 5
+
+       int resultado =24 % 5;
+       double resultado2= (7/2)+2.5;
+       double resultado3=(10.8/2) + 2;
+       int resultado4= ( 4 + 6 ) * 3 + 2 * ( 5 - 1 );
+       int resultado5= (5 / 2) + (17 % 3);
+       boolean resultado6= 7 >= 5 ||27 != 8;
+       boolean resultado7= (45 <=7)!=(5>=7);
+       int resultado8= (27% 4)+(15/4);
+       int resultado9= (37/4)*(4-2);
+       boolean resultado10=(25>=7)||!(7<=2);
+       boolean resultado11=('H'<'J')&&('9'!='7');
+       boolean resultado12=25>20||13>5;
+       boolean resultado13=(10+4<(15-3))||((2*5)+1>(14-(2*2)));
+       boolean resultado14=(4*2<=8)||(2*2<5)&&(4>3+1);
+       boolean resultado15=(10<=2*5)&&(3<4)!=(8>7)&&(3*2<=4*-1);
+
+
+
+        System.out.println("El resultado de 24 % 5 es:" +resultado);
+        System.out.println("El resultado de 7/2+2.5 es:" +resultado2);
+        System.out.println("El resultado de 10.8/2+2 es:" +resultado3);
+        System.out.println("El resultado de ( 4 + 6 ) * 3 + 2 * ( 5 - 1)es:" +resultado4);
+        System.out.println("El resultado de (5 / 2) + (17 % 3)es:" +resultado5);
+        System.out.println("El resultado de 7 >= 5 ||27 != 8 es:" +resultado6);
+        System.out.println("El resultado de (45 <=7)!=(5>=7)es:" +resultado7);
+        System.out.println("El resultado de (27% 4)+(15/4)es:" +resultado8);
+        System.out.println("El resultado de (37/4)*(4-2)es:" +resultado9);
+        System.out.println("El resultado de (25>=7)||!(7<=2)es:" +resultado10);
+        System.out.println("El resultado de ('H'<'J')&&('9'!='7')es:" +resultado11);
+        System.out.println("El resultado de 25>20||13>5 es:" +resultado12);
+        System.out.println("El resultado de (10+4<(15-3))||((2*5)+1>(14-(2*2))) es:" +resultado13);
+        System.out.println("El resultado de (4*2<=8)||(2*2<5)&&(4>3+1) es:" +resultado14);
+        System.out.println("El resultado de (10<=2*5)&&(3<4)!=(8>7)&&(3*2<=4*-1) es:" +resultado15);
+
+
+
+
+
+    }
+    }
