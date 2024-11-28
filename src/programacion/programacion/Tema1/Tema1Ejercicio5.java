9e444cc5e1249a76b9bf70ea9ddff5b511937afc
voski029 <diamamail557@gmail.com>
2024-11-28 12:00:03 +0100

    probando a ver si se actualiza

--- src/programacion/programacion/Tema1/Tema1Ejercicio5.java
+++ src/programacion/programacion/Tema1/Tema1Ejercicio5.java
@@ -4,21 +4,21 @@ public class Tema1Ejercicio5 {
     public static void main(String[] args) {
         // Realizar operacion 24 % 5
 
-       int resultado =24 % 5;
-       double resultado2= (7/2)+2.5;
-       double resultado3=(10.8/2) + 2;
-       int resultado4= ( 4 + 6 ) * 3 + 2 * ( 5 - 1 );
-       int resultado5= (5 / 2) + (17 % 3);
-       boolean resultado6= 7 >= 5 ||27 != 8;
-       boolean resultado7= (45 <=7)!=(5>=7);
-       int resultado8= (27% 4)+(15/4);
-       int resultado9= (37/4)*(4-2);
-       boolean resultado10=(25>=7)||!(7<=2);
-       boolean resultado11=('H'<'J')&&('9'!='7');
-       boolean resultado12=25>20||13>5;
-       boolean resultado13=(10+4<(15-3))||((2*5)+1>(14-(2*2)));
-       boolean resultado14=(4*2<=8)||(2*2<5)&&(4>3+1);
-       boolean resultado15=(10<=2*5)&&(3<4)!=(8>7)&&(3*2<=4*-1);
+        int resultado =24 % 5;
+        double resultado2= (7/2)+2.5;
+        double resultado3=(10.8/2) + 2;
+        int resultado4= ( 4 + 6 ) * 3 + 2 * ( 5 - 1 );
+        int resultado5= (5 / 2) + (17 % 3);
+        boolean resultado6= 7 >= 5 ||27 != 8;
+        boolean resultado7= (45 <=7)!=(5>=7);
+        int resultado8= (27% 4)+(15/4);
+        int resultado9= (37/4)*(4-2);
+        boolean resultado10=(25>=7)||!(7<=2);
+        boolean resultado11=('H'<'J')&&('9'!='7');
+        boolean resultado12=25>20||13>5;
+        boolean resultado13=(10+4<(15-3))||((2*5)+1>(14-(2*2)));
+        boolean resultado14=(4*2<=8)||(2*2<5)&&(4>3+1);
+        boolean resultado15=(10<=2*5)&&(3<4)!=(8>7)&&(3*2<=4*-1);
 
 
 
@@ -43,4 +43,4 @@ public class Tema1Ejercicio5 {
 
 
     }
-    }
+}
